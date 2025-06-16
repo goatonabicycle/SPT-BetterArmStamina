5a45dbc868994365716786901ff615c87a60acdd
Rowan <malice.peanutbutter@gmail.com>
2025-06-16 18:09:36 +0200

    Initial working version

---
+++ Patches/MoreRealisticArmStamina.cs
@@ -0,0 +1,64 @@
+﻿using EFT;
+using HarmonyLib;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using static EFT.Player;
+using EFT.Animations;
+using System;
+
+namespace MoreRealisticArmStamina.Patches
+{
+    internal class ArmStaminaPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Player), nameof(Player.LateUpdate));
+        }
+
+        private static bool _wasAimingPreviously = false;
+        private static float _originalMultiplier;
+        private static bool _hasStoredOriginalMultiplier = false;
+
+        [PatchPrefix]
+        static void Prefix(ref Player __instance)
+        {
+            if (!Plugin.ModEnabled.Value || !__instance.IsYourPlayer)
+                return;
+
+            ProceduralWeaponAnimation pwa = __instance.ProceduralWeaponAnimation;
+            if (pwa == null)
+                return;
+
+            bool isPlayerAiming = pwa.IsAiming;
+            float currentMultiplier = __instance.Physical.HandsStamina.Multiplier;
+
+            if (!_hasStoredOriginalMultiplier)
+            {
+                _originalMultiplier = currentMultiplier;
+                _hasStoredOriginalMultiplier = true;
+            }
+
+            if (isPlayerAiming)
+            {
+                float drainPercent = __instance.Pose switch
+                {
+                    EPlayerPose.Prone => Plugin.ProneAdsDrainPercent.Value,
+                    EPlayerPose.Duck => Plugin.CrouchAdsDrainPercent.Value,
+                    _ => 100f
+                };
+
+                float newMultiplier = drainPercent / 100f;
+                if (Math.Abs(currentMultiplier - newMultiplier) > 0.001f)
+                {
+                    __instance.Physical.HandsStamina.Multiplier = newMultiplier;
+                }
+            }
+            else if (_wasAimingPreviously)
+            {
+                __instance.Physical.HandsStamina.Multiplier = _originalMultiplier;
+            }
+
+            _wasAimingPreviously = isPlayerAiming;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using MoreRealisticArmStamina.Patches;
+
+namespace MoreRealisticArmStamina
+{
+    [BepInPlugin("goatonabicycle.MoreRealisticArmStamina", "MoreRealisticArmStamina", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ManualLogSource LogSource;
+        public static ConfigEntry<bool> ModEnabled;
+        public static ConfigEntry<float> ProneAdsDrainPercent;
+        public static ConfigEntry<float> CrouchAdsDrainPercent;
+
+        private void Awake()
+        {
+            LogSource = Logger;
+            LogSource.LogInfo("MoreRealisticArmStamina Plugin loaded!");
+
+            ModEnabled = Config.Bind(
+                "General",
+                "Enabled",
+                true,
+                "Should work or should not work."
+            );
+
+            ProneAdsDrainPercent = Config.Bind(
+                "ADS Stamina Adjustment",
+                "ProneAdsDrainPercent",
+                30f,
+                "ADS arm stamina drain rate as a percentage of normal when prone. (e.g., 30 for 30%, 100 for 100%)"
+            );
+
+            CrouchAdsDrainPercent = Config.Bind(
+                "ADS Stamina Adjustment",
+                "CrouchAdsDrainPercent",
+                60f,
+                "ADS arm stamina drain rate as a percentage of normal when crouching. (e.g., 60 for 60%, 100 for 100%)"
+            );
+
+            new ArmStaminaPatch().Enable();
+        }
+    }
+}
